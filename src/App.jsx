c2808883a8fb5c950e319fada95e803c75bc8d92
shashanksoni61 <shashanksoni61@gmail.com>
2022-04-20 09:58:08 +0530

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -7,46 +7,25 @@ import { AppContextProvider, AppContext } from "./context/AppContext";
 
 function Homepage() {
   const { summaryData } = useContext(AppContext);
-  const ths = (
-    <tr>
-      <th>P1</th>
-      <th>P2</th>
-      <th>P3</th>
-      <th>P4</th>
-      <th>P5</th>
-      <th>P6</th>
-    </tr>
-  );
-
-  const rows = summaryData.map((item) => (
-    <tr key={item.p2}>
-      <td>{item.p2}</td>
-      <td>{item.p3}</td>
-      <td>{item.p4}</td>
-      <td>{item.p5}</td>
-      <td>
-        {item.p6.options.join(", ")}
-        {"\n"}
-        {item.p6.probDetail}
-      </td>
-      <td>{item.p7}</td>
-    </tr>
-  ));
+  const [formData, setFormData] = useState();
 
   return (
     <>
-      {summaryData.length > 0 && (
-        <Table striped highlightOnHover>
-          <thead>{ths}</thead>
-          <tbody>{rows}</tbody>
-        </Table>
-      )}
-      <Form />
-      <div className="additional-buttons">
-        <Button className="btn btn-lg ml-20">Back</Button>
-        <Link to="/summary">
-          <Button className="btn btn-lg">Next</Button>
-        </Link>
+      <div className="app">
+        <div className="heading">
+          <h2>Pain & Functional Description</h2>
+          <p>
+            The description of the current situation gives your Optimum Trainer
+            a picture of and clues to the underlying causes of your problems
+          </p>
+        </div>
+        <Form />
+        <div className="additional-buttons">
+          <Button className="btn btn-lg ml-20">Back</Button>
+          <Link to="/summary">
+            <Button className="btn btn-lg">Next</Button>
+          </Link>
+        </div>
       </div>
     </>
   );
@@ -56,12 +35,14 @@ function SummaryPage() {
   const { summaryData } = useContext(AppContext);
   const ths = (
     <tr>
-      <th>P1</th>
-      <th>P2</th>
-      <th>P3</th>
-      <th>P4</th>
-      <th>P5</th>
-      <th>P6</th>
+      <th>Have you been diagnosed with this problem?</th>
+      <th>Did the problem start after a physical trauma?</th>
+      <th>Did the problem start after a mental trauma?</th>
+      <th>How often do you experience the problem?</th>
+      <th>When do you experience the problem?</th>
+      <th>
+        How intense is the experience of the problem on average on a 0-10 scale?
+      </th>
     </tr>
   );
 
@@ -73,7 +54,8 @@ function SummaryPage() {
       <td>{item.p5}</td>
       <td>
         {item.p6.options.join(", ")}
-        {"\n"}
+        <br />
+        <b>Additional Information &nbsp;</b>
         {item.p6.probDetail}
       </td>
       <td>{item.p7}</td>
@@ -81,15 +63,24 @@ function SummaryPage() {
   ));
   return (
     <>
-      <p>Summary Page</p>
-      <Table striped highlightOnHover>
-        <thead>{ths}</thead>
-        <tbody>{rows}</tbody>
-      </Table>
-      <div className="additional-buttons">
-        <Link to="/">
-          <Button className="btn btn-lg ml-20">Back</Button>
-        </Link>
+      <div className="App">
+        <div className="heading">
+          <h2>Pain & Functional Description</h2>
+          <p>
+            The description of the current situation gives your Optimum Trainer
+            a picture of and clues to the underlying causes of your problems
+          </p>
+        </div>
+        <p>Summary Page</p>
+        <Table striped highlightOnHover>
+          <thead>{ths}</thead>
+          <tbody>{rows}</tbody>
+        </Table>
+        <div className="additional-buttons">
+          <Link to="/">
+            <Button className="btn btn-lg ml-20">Back</Button>
+          </Link>
+        </div>
       </div>
     </>
   );
@@ -99,20 +90,10 @@ function App() {
   return (
     <BrowserRouter>
       <AppContextProvider>
-        <div className="App">
-          <div className="heading">
-            <h2>Pain & Functional Description</h2>
-            <p>
-              The description of the current situation gives your Optimum
-              Trainer a picture of and clues to the underlying causes of your
-              problems
-            </p>
-          </div>
-          <Routes>
-            <Route path="/" element={<Homepage />} />
-            <Route path="/summary" element={<SummaryPage />} />
-          </Routes>
-        </div>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/summary" element={<SummaryPage />} />
+        </Routes>
       </AppContextProvider>
     </BrowserRouter>
   );
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -10,7 +10,7 @@ import {
 import { AppContext } from "../context/AppContext";
 
 const Form = ({ pushDataToSummary }) => {
-  const { setSummaryData } = useContext(AppContext);
+  const { summaryData, setSummaryData } = useContext(AppContext);
   const [formValues, setFormValues] = useState({
     p1: "",
     p2: "",
@@ -24,20 +24,47 @@ const Form = ({ pushDataToSummary }) => {
     p7: "",
   });
 
-  const { p1, p2, p3, p4, p5, p6, p7 } = formValues;
-  const { options, probDetail } = p6;
-
-  console.log(formValues);
-
-  const submitData = () => {
-    if (!p2 || !p3 || !p4 || !p5 || options.length === 0 || !p7) {
-      console.log("fill all values");
-      return;
+  const [forms, setForms] = useState(
+    summaryData.length > 0
+      ? summaryData
+      : [
+          {
+            p1: "",
+            p2: "",
+            p3: "",
+            p4: "",
+            p5: "",
+            p6: {
+              options: [],
+              probDetail: "",
+            },
+            p7: "",
+          },
+        ]
+  );
+  const handleFormChange = (
+    value,
+    field,
+    index,
+    type = "input",
+    checboxField = ""
+  ) => {
+    const oldData = [...forms];
+    if (type === "input") {
+      oldData[index][field] = value;
+    } else {
+      if (checboxField === "options") {
+        oldData[index][field][checboxField] = value;
+      } else {
+        oldData[index][field][checboxField] = value.target.value;
+      }
     }
+    setForms(oldData);
+    setSummaryData(oldData);
+  };
 
-    setSummaryData((prev) => [...prev, formValues]);
-    setFormValues((prev) => ({
-      ...prev,
+  const addFormFields = () => {
+    let formObj = {
       p1: "",
       p2: "",
       p3: "",
@@ -48,8 +75,36 @@ const Form = ({ pushDataToSummary }) => {
         probDetail: "",
       },
       p7: "",
-    }));
+    };
+    setForms([...forms, formObj]);
   };
+
+  console.log(forms);
+
+  const { p1, p2, p3, p4, p5, p6, p7 } = formValues;
+  const { options, probDetail } = p6;
+
+  // const submitData = () => {
+  //   if (!p2 || !p3 || !p4 || !p5 || options.length === 0 || !p7) {
+  //     console.log("fill all values");
+  //     return;
+  //   }
+
+  //   setSummaryData((prev) => [...prev, formValues]);
+  //   setFormValues((prev) => ({
+  //     ...prev,
+  //     p1: "",
+  //     p2: "",
+  //     p3: "",
+  //     p4: "",
+  //     p5: "",
+  //     p6: {
+  //       options: [],
+  //       probDetail: "",
+  //     },
+  //     p7: "",
+  //   }));
+  // };
 
   return (
     <>
@@ -60,172 +115,209 @@ const Form = ({ pushDataToSummary }) => {
             functional problems, describe this/these below. (List the symptoms
             in descending order with the mose troublesome first)
           </p>
-          <Textarea
-            disabled
-            name=""
-            id=""
-            cols="100"
-            rows="3"
-            value={p1}
-            onChange={(e) =>
-              setFormValues((prev) => ({ ...prev, p1: e.target.value }))
-            }
-          ></Textarea>
-          <div className="questions-section">
-            <div className="question">
-              <p>Have you been diagnosed with this problem?</p>
-              <div className="question-right">
-                <RadioGroup
-                  required
-                  value={p2}
-                  onChange={(e) =>
-                    setFormValues((prev) => ({ ...prev, p2: e }))
-                  }
-                >
-                  <Radio value="Not relevant" label="Not relevant" />
-                  <Radio value="Yes" label="Yes" />
-                  <Radio value="No" label="No" />
-                </RadioGroup>
-              </div>
-            </div>
-            {/*  */}
-            <div className="question">
-              <p>Did the problem start after a physical trauma?</p>
-              <div className="question-right">
-                <RadioGroup
-                  required
-                  value={p3}
-                  onChange={(e) =>
-                    setFormValues((prev) => ({ ...prev, p3: e }))
-                  }
-                >
-                  <Radio value="Not relevant" label="Not relevant" />
-                  <Radio value="Yes" label="Yes" />
-                  <Radio value="No" label="No" />
-                </RadioGroup>
-              </div>
-            </div>
-            {/*  */}
-            <div className="question">
-              <p>Did the problem start after a mental trauma?</p>
-              <div className="question-right">
-                <RadioGroup
-                  required
-                  value={p4}
-                  onChange={(e) =>
-                    setFormValues((prev) => ({ ...prev, p4: e }))
-                  }
-                >
-                  <Radio value="Not relevant" label="Not relevant" />
-                  <Radio value="Yes" label="Yes" />
-                  <Radio value="No" label="No" />
-                </RadioGroup>
-              </div>
-            </div>
-            {/*  */}
-            <div className="question flex-reverse align-items-flex-start">
-              <p>How often do you experience the problem?</p>
-              <div className="q-4-options">
-                <RadioGroup
-                  required
-                  value={p5}
-                  onChange={(e) =>
-                    setFormValues((prev) => ({ ...prev, p5: e }))
-                  }
-                >
-                  <Radio value="Not relevant" label="Not relevant" />
-                  <Radio value="Daily" label="Daily" />
-                  <Radio
-                    value="Several times/week"
-                    label="Several times/week"
-                  />
-                  <Radio value="A few times/month" label="A few times/month" />
-                  <Radio value="A few times/year" label="A few times/year" />
-                </RadioGroup>
-              </div>
-            </div>
-            <div className="question flex-reverse align-items-flex-start">
-              <p>When do you experience the problem??</p>
-              <div className="five">
-                <div className="five-left">
-                  <CheckboxGroup
-                    required
-                    value={options}
-                    onChange={(e) =>
-                      setFormValues((prev) => ({
-                        ...prev,
-                        p6: {
-                          ...prev.p6,
-                          options: [...e],
-                        },
-                      }))
-                    }
-                  >
-                    <Checkbox value="Not relevant" label="Not relevant" />
-                    <Checkbox value="When lying down" label="When lying down" />
-                    <Checkbox value="When sitting" label="When sitting" />
-                    <Checkbox value="Under standing" label="Under standing" />
-                    <Checkbox value="In walking" label="In walking" />
-                  </CheckboxGroup>
+          {forms.map((item, index) => (
+            <div key={index}>
+              <Textarea
+                disabled
+                name=""
+                id=""
+                cols="100"
+                rows="3"
+                value={p1}
+                onChange={(e) =>
+                  setFormValues((prev) => ({ ...prev, p1: e.target.value }))
+                }
+              ></Textarea>
+              <div className="questions-section">
+                <div className="question">
+                  <p>Have you been diagnosed with this problem?</p>
+                  <div className="question-right">
+                    <RadioGroup
+                      required
+                      value={item.p2}
+                      onChange={(e) => {
+                        handleFormChange(e, "p2", index);
+                        setFormValues((prev) => ({ ...prev, p2: e }));
+                      }}
+                    >
+                      <Radio value="Not relevant" label="Not relevant" />
+                      <Radio value="Yes" label="Yes" />
+                      <Radio value="No" label="No" />
+                    </RadioGroup>
+                  </div>
                 </div>
-                <div className="five-right">
-                  <Textarea
-                    value={probDetail}
-                    onChange={(e) =>
-                      setFormValues((prev) => ({
-                        ...prev,
-                        p6: {
-                          ...prev.p6,
-                          probDetail: e.target.value,
-                        },
-                      }))
-                    }
-                    type="text"
-                    placeholder="Other? For example in rotations, side bends, wing stairs, when working with the arms above the head."
-                  />
+                {/*  */}
+                <div className="question">
+                  <p>Did the problem start after a physical trauma?</p>
+                  <div className="question-right">
+                    <RadioGroup
+                      required
+                      value={item.p3}
+                      onChange={(e) => {
+                        handleFormChange(e, "p3", index);
+                        setFormValues((prev) => ({ ...prev, p3: e }));
+                      }}
+                    >
+                      <Radio value="Not relevant" label="Not relevant" />
+                      <Radio value="Yes" label="Yes" />
+                      <Radio value="No" label="No" />
+                    </RadioGroup>
+                  </div>
                 </div>
-              </div>
-            </div>
-            <div className="question flex-reverse align-items-flex-start">
-              <p>
-                How intense is the experience of the problem on average on a
-                0-10 scale?
-              </p>
-              <div className="six-options">
-                <RadioGroup
-                  required
-                  value={p7}
-                  onChange={(e) =>
-                    setFormValues((prev) => ({
-                      ...prev,
-                      p7: e,
-                    }))
-                  }
-                >
-                  {/* {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((item) => (
+                {/*  */}
+                <div className="question">
+                  <p>Did the problem start after a mental trauma?</p>
+                  <div className="question-right">
+                    <RadioGroup
+                      required
+                      value={item.p4}
+                      onChange={(e) => {
+                        handleFormChange(e, "p4", index);
+                        setFormValues((prev) => ({ ...prev, p4: e }));
+                      }}
+                    >
+                      <Radio value="Not relevant" label="Not relevant" />
+                      <Radio value="Yes" label="Yes" />
+                      <Radio value="No" label="No" />
+                    </RadioGroup>
+                  </div>
+                </div>
+                {/*  */}
+                <div className="question flex-reverse align-items-flex-start">
+                  <p>How often do you experience the problem?</p>
+                  <div className="q-4-options">
+                    <RadioGroup
+                      required
+                      value={item.p5}
+                      onChange={(e) => {
+                        handleFormChange(e, "p5", index);
+                        setFormValues((prev) => ({ ...prev, p5: e }));
+                      }}
+                    >
+                      <Radio value="Not relevant" label="Not relevant" />
+                      <Radio value="Daily" label="Daily" />
+                      <Radio
+                        value="Several times/week"
+                        label="Several times/week"
+                      />
+                      <Radio
+                        value="A few times/month"
+                        label="A few times/month"
+                      />
+                      <Radio
+                        value="A few times/year"
+                        label="A few times/year"
+                      />
+                    </RadioGroup>
+                  </div>
+                </div>
+                <div className="question flex-reverse align-items-flex-start">
+                  <p>When do you experience the problem??</p>
+                  <div className="five">
+                    <div className="five-left">
+                      <CheckboxGroup
+                        required
+                        name="p6"
+                        value={item.p6.options}
+                        onChange={(e) => {
+                          console.log(e);
+                          handleFormChange(
+                            e,
+                            "p6",
+                            index,
+                            "checkbox",
+                            "options"
+                          );
+                          setFormValues((prev) => ({
+                            ...prev,
+                            p6: {
+                              ...prev.p6,
+                              options: [...e],
+                            },
+                          }));
+                        }}
+                      >
+                        <Checkbox value="Not relevant" label="Not relevant" />
+                        <Checkbox
+                          value="When lying down"
+                          label="When lying down"
+                        />
+                        <Checkbox value="When sitting" label="When sitting" />
+                        <Checkbox
+                          value="Under standing"
+                          label="Under standing"
+                        />
+                        <Checkbox value="In walking" label="In walking" />
+                      </CheckboxGroup>
+                    </div>
+                    <div className="five-right">
+                      <Textarea
+                        value={item.p6.probDetail}
+                        onChange={(e) => {
+                          handleFormChange(
+                            e,
+                            "p6",
+                            index,
+                            "checkbox",
+                            "probDetail"
+                          );
+                          setFormValues((prev) => ({
+                            ...prev,
+                            p6: {
+                              ...prev.p6,
+                              probDetail: e.target.value,
+                            },
+                          }));
+                        }}
+                        type="text"
+                        placeholder="Other? For example in rotations, side bends, wing stairs, when working with the arms above the head."
+                      />
+                    </div>
+                  </div>
+                </div>
+                <div className="question flex-reverse align-items-flex-start">
+                  <p>
+                    How intense is the experience of the problem on average on a
+                    0-10 scale?
+                  </p>
+                  <div className="six-options">
+                    <RadioGroup
+                      required
+                      value={item.p7}
+                      onChange={(e) => {
+                        handleFormChange(e, "p7", index);
+                        setFormValues((prev) => ({
+                          ...prev,
+                          p7: e,
+                        }));
+                      }}
+                    >
+                      {/* {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((item) => (
                 <Radio key={item} value={item} label={item} />
               ))} */}
 
-                  <Radio value="1" label="1" />
-                  <Radio value="2" label="2" />
-                  <Radio value="3" label="3" />
-                  <Radio value="4" label="4" />
-                  <Radio value="5" label="5" />
-                  <Radio value="6" label="6" />
-                  <Radio value="7" label="7" />
-                  <Radio value="8" label="8" />
-                  <Radio value="9" label="9" />
-                  <Radio value="10" label="10" />
-                </RadioGroup>
+                      <Radio value="1" label="1" />
+                      <Radio value="2" label="2" />
+                      <Radio value="3" label="3" />
+                      <Radio value="4" label="4" />
+                      <Radio value="5" label="5" />
+                      <Radio value="6" label="6" />
+                      <Radio value="7" label="7" />
+                      <Radio value="8" label="8" />
+                      <Radio value="9" label="9" />
+                      <Radio value="10" label="10" />
+                    </RadioGroup>
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
+          ))}
         </div>
       </div>
       <div className="plus-box">
         <hr className="plus-box-line" />
         <div className="fixed-plus">
-          <Button className="btn plus-btn round" onClick={submitData}>
+          <Button className="btn plus-btn round" onClick={addFormFields}>
             <span className="plus-button-span">&#x002B;</span>
           </Button>
         </div>
